15cd948e4dd97b50e1e2aa6d7eff2765dedf5620
ilahaka77 <ahmadarifilahaka77@gmail.com>
2020-11-17 14:17:34 +0700

    update

--- A-Store/app/Http/Controllers/APIProductController.php
+++ A-Store/app/Http/Controllers/APIProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Kategori;
+use App\Store;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
